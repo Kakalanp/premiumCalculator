4060a4a918d4c4edacad011eb47bbf6540ae648a
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-10-01 19:01:03 -0500

    Finished core requirements

--- back-end/premiums.js
+++ back-end/premiums.js
@@ -1,1 +1,68 @@
+const data = require("./premiumList.json");
 
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+const corsOptions = {
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+function ageCalculator(date) {
+  [year, month, day] = date.split('-');
+
+  const today = new Date();
+  let ageValue = today.getFullYear() - year;
+
+  if(today.getMonth() + 1 === parseInt(month)) {
+    if(today.getDate() < parseInt(day)) ageValue -= 1;
+  } else if(today.getMonth() + 1 < parseInt(month)) {
+    ageValue -= 1;
+  }
+
+  return ageValue;
+}
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("go to /api/v1/states to see the list of states available");
+});
+
+app.get("/api/v1/states", (req, res) => {
+  res.send(Object.keys(data));
+});
+
+app.get("/api/v1/states/:state&:plan&:date&:age", (req, res) => {
+  const options = [];
+
+  data[`${req.params.state}`][`${req.params.plan}`]
+    .concat(data["0"][`${req.params.plan}`])
+    .map((option) => {
+      if(parseInt(req.params.age) === ageCalculator(req.params.date)) {
+        const month = parseInt((req.params.date).substring(5,2));
+
+        if (option[0] === month || option[0] === 0) {
+          (parseInt(req.params.age) >= option[1] && parseInt(req.params.age) <= option[2]) &&
+          options.push({
+            "carrier": option[4],
+            "premium": option[3]
+          });
+        }
+      } else {
+        res.status(403).send('Date and age do not match');
+      }
+    });
+
+  res.send(options);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Listening to ${port}`));
---
+++ front-end/index.js
@@ -0,0 +1,110 @@
+const date = document.getElementById('birth');
+const state = document.getElementById('state');
+const plan = document.getElementById('plan');
+const age = document.getElementById('age');
+const button = document.getElementById('button');
+const period = document.getElementById('period');
+const premiumList = document.querySelector('.premium-list');
+
+let premiums = [];
+
+const api = 'http://localhost:3000/api/v1';
+
+// helper functions
+
+function calculateAge(date) {
+  [year, month, day] = date.split('-');
+
+  const today = new Date();
+  let ageValue = today.getFullYear() - year;
+
+  if(today.getMonth() + 1 === parseInt(month)) {
+    if(today.getDate() < parseInt(day)) ageValue -= 1;
+  } else if(today.getMonth() + 1 < parseInt(month)) {
+    ageValue -= 1;
+  }
+
+  return ageValue;
+}
+
+function addState(stateName) {
+  if(stateName !== '0'){
+    const [postal, name] = stateName.split('-');
+    name.replace(/([A-Z])/g, ' $1').trim().substring(0, 12);
+
+    state.innerHTML += `<option value="${stateName}">${postal} - ${name}</option>`;
+  }
+}
+
+function addPremium(premium) {
+  premiumList.innerHTML += `
+    <li class="item">
+      <p>${premium.carrier}</p>
+      <p>${premium.premium}</p>
+      <p>${(premium.premium * period.value).toFixed(2)}</p>
+      <p>${(premium.premium * period.value / 12).toFixed(2)}</p>
+    </li>
+  `
+}
+
+function updatePremiumList() {
+  premiumList.innerHTML = '';
+  premiums.forEach((premium) => {
+    addPremium(premium);
+  });
+}
+
+// API interactions
+
+function getStates() {
+  fetch(`${api}/states`,
+  { method: 'GET',
+    headers: { 'Content-type': 'application/json'},
+  })
+    .then((response) => response.json())
+    .then((data) => data.forEach(state => {
+      addState(state);
+    }));
+};
+
+function getPremiums() {
+  fetch(`${api}/states/${state.value}&${plan.value}&${date.value}&${age.value}`,
+  { method: 'GET',
+    headers: { 'Content-type': 'application/json'},
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      period.disabled = false
+      premiums = data;
+      updatePremiumList();
+    });
+};
+
+// Event listeners
+
+date.addEventListener('change', () => {
+  ageValue = calculateAge(date.value);
+
+  if (ageValue < 18){
+    button.disabled = true
+    age.classList.add('error');
+    age.value = 'You need to be older than 18';
+  }  else {
+    button.disabled = false
+    age.classList.remove('error');
+    age.value = ageValue;
+  }
+});
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  getPremiums();
+});
+
+period.addEventListener('change', () => {
+  updatePremiumList();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  getStates();
+});
